c8fca017a32c9bc9c6ab85e8175d89ce910a7782
SalustianoPosse-web <salustianoposse@gmail.com>
2025-11-14 16:22:02 -0300

    Delete TrabajoPractico4 directory
    Remove unnecessary folder

--- TrabajoPractico4/src/Punto2/ComparatorProducto.java
+++
@@ -1,9 +0,0 @@
-package Punto2;
-
-import java.util.Comparator;
-public class ComparatorProducto implements Comparator <Producto> {
-    @Override
-    public int compare(Producto o1, Producto o2) {
-        return o1.nombre.compareToIgnoreCase(o2.nombre);
-    }
-}
